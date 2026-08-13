6e9f7eafc0432d3dc3ab35571f6105be33ce472f
Alina Babenko <alinababenko.work@gmail.com>
2026-08-14 00:02:14 +0300

    Update default public IP variable in variables.tf

--- terraform/vm/network/variables.tf
+++ terraform/vm/network/variables.tf
@@ -2,7 +2,7 @@ variable "base_name"           { default = "Alina-module" }
 
 variable "my_public_ip" {
     type        = string
-    default     = "212.178.18.103" # ПОДСТАВЬ СЮДА СВОЙ IP (например, "178.20.30.40")
+    default     = "212.178.18.103"
     description = "Мой публичный IP для ограничения доступа по SSH"
 }
 variable "resource_group_name" {}
